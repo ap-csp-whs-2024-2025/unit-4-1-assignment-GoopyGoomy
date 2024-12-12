0b081202c9c5c514af9f438a7d9b072ff98f9926
GoopyGoomy <earlgreytea0627@gmail.com>
2024-12-12 21:45:36 +0000

    exercise 1 done

--- classwork.cpp
+++ classwork.cpp
@@ -2,14 +2,28 @@
 #include <string>
 
 int main()
-{
-  int age = 10; // SET age TO 10
-  double price = 19.99;
-  bool isRaining = false;
+{ int x; //exercise 1
+int y;
+int z;
+int add_xy;
+int sub_yx;
+int mult_xy;
+int div_xy;
+bool greater_than;
+bool less_than;
 
-  std::cout << age << "\n" << price << std::endl << isRaining << std::endl;
-// std::endl and \n puts stuff on another line
-int grade_level = 12;
-std::cout << "I am a " << grade_level << "th grader" << std::endl;
+x = 25;
+y = 3;
+z = 10;
+add_xy = x+y;
+sub_yx = y-x;
+mult_xy = x*y;
+div_xy = x/y;
+greater_than = x>z;
+less_than = y<x;
+
+std::cout << add_xy << "," << sub_yx << "," << mult_xy << "," << div_xy << "," << greater_than << "," << less_than << "\n" << "\n";
+
+
   return 0;
 }
